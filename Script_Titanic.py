2f4b01fa0d3dd2ad4ef7ea777d0a72814b272e1e
Clara <cla.ra@hotmail.fr>
2016-11-02 11:56:51 +0100

    add fare content

--- Script_Titanic.py
+++ Script_Titanic.py
@@ -42,6 +42,23 @@ titanic_test.loc[titanic_test["Embarked"] == "C","Embarked"] = 1
 titanic_test.loc[titanic_test["Embarked"] == "Q","Embarked"] = 2
 
 ## Fare
-print(titanic_train["Fare"].describe()) # no missing values
- # master
- 
+print(titanic_train["Fare"].describe()) # no missing values 
+test_df["Fare"].fillna(test_df["Fare"].median(), inplace=True)
+
+# convert from float to int
+titanic_df['Fare'] = titanic_df['Fare'].astype(int)
+test_df['Fare']    = test_df['Fare'].astype(int)
+
+# get fare for survived & didn't survive passengers 
+fare_not_survived = titanic_df["Fare"][titanic_df["Survived"] == 0]
+fare_survived     = titanic_df["Fare"][titanic_df["Survived"] == 1]
+
+# get average and std for fare of survived/not survived passengers
+avgerage_fare = DataFrame([fare_not_survived.mean(), fare_survived.mean()])
+std_fare      = DataFrame([fare_not_survived.std(), fare_survived.std()])
+
+# plot
+titanic_df['Fare'].plot(kind='hist', figsize=(15,3),bins=100, xlim=(0,50))
+
+avgerage_fare.index.names = std_fare.index.names = ["Survived"]
+avgerage_fare.plot(yerr=std_fare,kind='bar',legend=False)
